f88ddc17722068a53b6627159cea8f784e2898f9
Irson <jiordesta@gmail.com>
2026-05-30 07:07:25 +0800

    feature: onboarding message

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller,  Post, UseGuards, Request, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO, RegisterUserDTO } from 'src/libs/dto';
 import { LocalAuthGuard } from './guard/local.guard';
@@ -6,23 +13,24 @@ import { JwtAuthGuard } from './guard/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
-    @Get('')
-    @UseGuards(JwtAuthGuard)
-    async test(@Request() request: any) {
-      const authHeader = request.headers.authorization;
-      const token = authHeader && authHeader.startsWith('Bearer ') 
-        ? authHeader.split(' ')[1] 
+  constructor(private readonly authService: AuthService) {}
+  @Get('')
+  @UseGuards(JwtAuthGuard)
+  async authenticate(@Request() request: any) {
+    const authHeader = request.headers.authorization;
+    const token =
+      authHeader && authHeader.startsWith('Bearer ')
+        ? authHeader.split(' ')[1]
         : null;
-      return await this.authService.authenticate(request.user, token);
-    }
-    @Post('register')
-    async registerUser(@Body() registerUserDTO: RegisterUserDTO) {
-      return await this.authService.register(registerUserDTO);
-    }
-    @Post('login')
-    @UseGuards(LocalAuthGuard)
-    async loginUser(@Request() request: any, @Body() loginDTO: LoginDTO) {
-      return await this.authService.login(request.user, loginDTO);
-    }    
+    return await this.authService.authenticate(request.user, token);
+  }
+  @Post('register')
+  async registerUser(@Body() registerUserDTO: RegisterUserDTO) {
+    return await this.authService.register(registerUserDTO);
+  }
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  async loginUser(@Request() request: any, @Body() loginDTO: LoginDTO) {
+    return await this.authService.login(request.user, loginDTO);
+  }
 }
--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,17 +1,28 @@
 import * as bcrypt from 'bcrypt';
 import { prisma } from '../db';
-export const createHashedPassword = async (password: string): Promise<string> => {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-}
-export const validatePassword = async (password: string, storedPassword: string): Promise<boolean> => {
-    return await bcrypt.compare(password, storedPassword);
-}
+export const createHashedPassword = async (
+  password: string,
+): Promise<string> => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+export const validatePassword = async (
+  password: string,
+  storedPassword: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(password, storedPassword);
+};
 export const getUserInfo = async (userId: number) => {
-    const userInfo = await prisma.user.findUnique({
-        where: { id: userId },
-        include: { userDetails: true, userCredential: true, userRole: true },
-    });
+  const userInfo = await prisma.user.findUnique({
+    where: { id: userId },
+    include: {
+      userDetails: true,
+      userCredential: true,
+      userRole: {
+        include: { role: true },
+      },
+    },
+  });
 
-    return userInfo
-}
+  return userInfo;
+};
